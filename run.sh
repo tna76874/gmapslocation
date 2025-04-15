5b6cda8273051feb4dbbce480f58d462be96d6f2
lmh <dev@hilberg.eu>
2025-04-15 17:04:24 +0200

    update docker-compose.yml

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+INTERVAL=${1:-5}
+
 docker run --rm \
   -v "$(pwd)/data:/app/data" \
-  -e INTERVAL_MINUTES=10 \
+  -e INTERVAL_MINUTES="$INTERVAL" \
   ghcr.io/tna76874/gmapslocation:latest
